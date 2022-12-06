f9244413b83ad17770f81fd323acf297a9c5d8ca
eulinaji <555890@my.cuhsd.org>
2022-12-06 00:21:21 -0800

    worked on checkpoint 3

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -18,18 +18,39 @@ public class FracCalc {
 
 
         // CHECKPOINT 2
+
+        String userInput;
+
         while (true) {
             Scanner in = new Scanner(System.in);
             System.out.print("Enter a fraction equation (type 'quit' to exit): ");
-            String userInput = in.next();
+            userInput = in.next();
 
             if (userInput == "quit") {
                 break;
             }
-
-            System.out.print(produceAnswer(userInput));
+            // System.out.print(produceAnswer(userInput));
         }
 
+        String finalFraction = " ";
+
+        String[] equationParts;
+        equationParts = splitEquation(userInput);
+
+        if (equationParts[0] == "+") {
+            finalFraction = addFractions(equationParts[1], equationParts[2]);
+        }
+        else if (equationParts[0] == "-") {
+            finalFraction = subtractFractions(equationParts[1], equationParts[2]);
+        }
+        else if (equationParts[0] == "*") {
+            finalFraction = multiplyFractions(equationParts[1], equationParts[2]);
+        }
+        else if (equationParts[0] == "/") {
+            finalFraction = divideFractions(equationParts[1], equationParts[2]);
+        }
+
+        System.out.print(finalFraction);
     }
 
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -63,6 +84,7 @@ public class FracCalc {
 
 
     // CHECKPOINT 2
+    /*
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
@@ -118,6 +140,111 @@ public class FracCalc {
 
         return result;
     }
+    */
+
 
     // TODO: Fill in the space below with any helper methods that you think you will need
+
+
+    public static String[] splitEquation (String input) {
+        Scanner in = new Scanner(input);
+        in.useDelimiter(" ");
+
+        String firstFrac = "";
+        String secondFrac = "";
+        String operator = "";
+        if (in.hasNext()) {
+            firstFrac = in.next();
+            operator = in.next();
+            secondFrac = in.next();
+        }
+
+        String[] result = {operator, firstFrac, secondFrac};
+        return result;
+    }
+
+
+
+    public static String[] getFractionParts (String fraction) {
+        String whole = "";
+        String frac = "";
+        String numerator = "";
+        String denominator = "";
+
+        Scanner in = new Scanner(fraction);
+        in.useDelimiter("_");
+        if (in.hasNext()) {
+            if ( fraction.contains("_") ) {
+                whole = in.next();
+                frac = in.next();
+
+                Scanner in1 = new Scanner(fraction);
+                in1.useDelimiter("/");
+                numerator = in1.next();
+                denominator = in1.next();
+            }
+            else if ( fraction.contains("/") == false ) {
+                whole = in.next();
+                numerator = "0";
+                denominator = "1";
+            }
+            else {
+                whole = "0";
+                frac = in.next();
+                Scanner in1 = new Scanner(fraction);
+                in1.useDelimiter("/");
+                numerator = in1.next();
+                denominator = in1.next();
+            }
+        }
+
+        else {
+            throw new InputMismatchException("No input");
+        }
+
+        String[] result = {whole, numerator, denominator};
+        return result;
+    }
+
+
+    public static String simplifyFractions(int improperNumer, int improperDenom) {
+
+
+        String simpleAnswer = "";
+        return simpleAnswer;    // "4_5/8"
+    }
+
+    public static String addFractions(String frac1, String frac2) {
+        String[] parts1 = getFractionParts(frac1);
+        int w1 = Integer.parseInt(parts1[0]);
+        int n1 = Integer.parseInt(parts1[1]);
+        int d1 = Integer.parseInt(parts1[2]);
+
+        String[] parts2 = getFractionParts(frac2);
+        int w2 = Integer.parseInt(parts2[0]);
+        int n2 = Integer.parseInt(parts2[1]);
+        int d2 = Integer.parseInt(parts2[2]);
+
+
+        int n = (w1 * d1 + n1) * d2 + (w2 * d2 + n2) * d1;
+        int d = d1 * d2;
+
+        String finalAnswer = simplifyFractions(n,d);
+
+        return finalAnswer;
+    }
+
+    public static String subtractFractions(String frac1, String frac2) {
+
+    }
+
+    public static String multiplyFractions(String frac1, String frac2) {
+
+    }
+
+    public static String divideFractions(String frac1, String frac2) {
+
+    }
+
+
 }
